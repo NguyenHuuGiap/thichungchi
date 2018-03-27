2ee2b77a03823f9730590b979082439ea9f9fde5
Nguyenhuugiap <nguyenhuugiap4@gmail.com>
2018-03-27 17:11:44 +0700

    create update show destroy index category

--- app/controllers/admin/category_controller.rb
+++ app/controllers/admin/category_controller.rb
@@ -1,13 +1,13 @@
 class Admin::CategoryController < Admin::BaseController
   before_action :load_categories, only: [:index, :destroy]
   before_action :load_category_new, only: [:new, :create]
+  before_action :load_category, only: [:edit, :update, :destroy, :show]
 
   def index;end
 
   def new;end
 
   def create
-    debugger
     @category.assign_attributes category_params
     if @category.save
       flash[:success] = "Thêm mới thành công"
@@ -17,17 +17,42 @@ class Admin::CategoryController < Admin::BaseController
     redirect_to admin_category_index_path
   end
 
+  def edit
+  end
+
+  def update
+    @category.assign_attributes category_params
+    if @category.save
+      flash[:success] = "Cập nhật thành công"
+    else
+      flash[:error] = "Cập nhật thất bại"
+    end
+    redirect_to admin_category_index_path
+  end
+
+  def destroy
+    if @category.destroy
+      flash[:success] = "Xóa thành công"
+    else
+      flash[:error] = "Xóa không thành công"
+    end
+    respond_to :js
+  end
+
   private
+  def load_category
+    @category = Category.find_by id: params[:id]
+  end
+
   def load_categories
     @categories = Category.all
   end
 
   def category_params
-    params.require(:category).permit :name, :description, :parent, location_categories_attributes: [:id, location_id: []]
+    params.require(:category).permit :name, :description, :parent
   end
 
   def load_category_new
     @category = Category.new
-    @category.location_categories.build
   end
 end
