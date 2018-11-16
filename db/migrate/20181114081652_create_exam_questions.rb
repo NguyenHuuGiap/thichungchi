150ffb98afba42f73a078cd348cf6869af75319b
Nguyenhuugiap <nguyenhuugiap4@gmail.com>
2018-11-16 17:39:09 +0700

    abc

--- db/migrate/20181114081652_create_exam_questions.rb
+++ db/migrate/20181114081652_create_exam_questions.rb
@@ -3,6 +3,7 @@ class CreateExamQuestions < ActiveRecord::Migration[5.1]
     create_table :exam_questions do |t|
       t.references :exam, foreign_key: true
       t.references :question, foreign_key: true
+      t.integer :choose_answer
 
       t.timestamps
     end
